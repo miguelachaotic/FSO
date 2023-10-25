5bdaddf4e68ef87109fc795948c296237f4b02d9
David Mieres Pérez <54892055+0david0mp@users.noreply.github.com>
2023-10-25 17:15:22 +0200

    Actualizar copia.c

--- c/copia.c
+++ c/copia.c
@@ -6,28 +6,24 @@
 
 int main (int argc, char ** argv)
 {
-	char* ruta0, * ruta1;
-	ruta0 = argv[1];
-	ruta1 = argv[2];
+  char* ruta0, * ruta1;
+	 ruta0 = argv[1];
+ 	ruta1 = argv[2];
 
-	FILE * pFich0, * pFich1;
+ 	FILE * pFich0, * pFich1;
   char a = 0;
 
-	if (argc != 3){
-		fprintf(stderr, "USO: copia [archivo_org] [archivo_dest]\n");
-		exit (EXIT_FAILURE);
-	}
-	else if (strcmp(ruta0, ruta1) == 0){
-		fprintf(stderr, "ERROR: los 2 archivos no pueden ser el mismo\n");
-		exit (EXIT_FAILURE);
-	}
-
-	else if ((pFich1 = fopen(ruta1, "r")) != NULL){
+	 if (argc != 3){
+	  	fprintf(stderr, "USO: copia [archivo_org] [archivo_dest]\n");
+  		exit (EXIT_FAILURE);
+ 	} else if (strcmp(ruta0, ruta1) == 0) {
+	  	fprintf(stderr, "ERROR: los 2 archivos no pueden ser el mismo\n");
+  		exit (EXIT_FAILURE);
+	 } else if ((pFich1 = fopen(ruta1, "r")) != NULL) {
     fprintf(stderr, "ERROR: el archivo destino ya existe\n");
-		fclose(pFich1);
+	  	fclose(pFich1);
     exit(EXIT_FAILURE);
-	}
-	else if ((pFich0 = fopen(ruta0, "r")) == NULL){
+  } else if ((pFich0 = fopen(ruta0, "r")) == NULL) {
     fprintf(stderr, "ERROR: el archivo origen no existe\n");
     exit(EXIT_FAILURE);
 	}
