a4c6e53f512a434e4336dfec72fc2bde09d5f89c
Miguel Ángel <119065677+miguelachaotic@users.noreply.github.com>
2023-11-02 23:56:49 +0100

    Added shell.c

--- shell.c
+++ shell.c
@@ -1,101 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 
-#define PROMPT "chiquishell~~> "
+#define PROMPT "<~~chiquishell~~> "
+#define EXIT_ERR 1
 #define MAX_CMD_LENGTH 256
-#define MAX_ARG_LENGTH 64
+#define MAX_ARGS 20
 
 
-void vaciar_buffer(char *buffer);
-
-int main(int argc, char** argv){
-    
-    char *buffer;
-    char *delimitador = " ";
-    char *token;
-    char **args;
-    int contador_args = 0;
-    int tam_antes;
-    pid_t pid;
-    const char * file;
-    int estado;
-
-
-    if(argc > 1){
-        fprintf(stderr, "USO: shell [sin argumentos]"); 
-    }
-   // Hasta que no se introduzca "salir" no se saldrá del shell.
-    while (1) 
-    {   
-
-        printf(PROMPT);
-        fflush(NULL);
-        buffer = malloc(MAX_CMD_LENGTH*sizeof(char));
-        read(0 , buffer, MAX_CMD_LENGTH*sizeof(char));     
-        // En este punto ya tenemos en el buffer la cadena obtenida por el usuario.
-        // Ahora comprobaremos si corresponde a alguna cadena de comando
-        buffer = strtok(buffer, "\n"); // Elimino el \n de la cadena
-        
-        
-        token = strtok(buffer, delimitador); // Cojo el primer argumento
-        tam_antes = sizeof(char*);
-        
-        args = (char**)malloc(tam_antes);
-        
-        while(token != NULL)
-        {   
-            args[contador_args] = token;
-            
-            contador_args++;
-
-            token = strtok(NULL, delimitador);
-        }
-
-        args[contador_args] = NULL;
-
-        if (args[0] == NULL) {
-            printf("Cadena vacia\n");
-        }
-
-
-        if(!strcmp(args[0], "copia"))
-        {
-            if ( fork () == 0 ) {
-                estado = execvp ("./copia", args);
-                printf ("ERROR: no se pudo ejecutar el comando %s\n", args[0]) ;
-                return estado;
-
-             } else {
-                 wait ( NULL );
-             }
-        } 
-        else if (!strcmp(args[0], "muestra"))
-        {
-            printf("Ejecuta muestra\n");
-        }
-        else if(!strcmp(args[0], "lista"))
-        {
-            printf("Ejecuta lista\n");
-        }
-        else if(!strcmp(args[0], "salir"))
-        {
-            free(buffer);
-            free(args);
-            return 0; // Termina el programa
-        }
-        else
-        {
-            printf("Comando desconocido\n");
-        }
-
-        vaciar_buffer(buffer);
-        contador_args = 0;
-    }
-}
 
 void vaciar_buffer(char *buffer)
 {
@@ -105,4 +20,207 @@ void vaciar_buffer(char *buffer)
     }
 }
 
+char* crear_buffer()
+{
+    char* nuevo_buffer;
+    nuevo_buffer = (char*)malloc(MAX_CMD_LENGTH*sizeof(char));
+    return nuevo_buffer;
+}
 
+void eliminar_buffer(char* buffer)
+{
+    free(buffer);
+}
+
+void print_args(char** args, int num_args)
+{
+    for(int i = 0; i < num_args; i++)
+    {
+        printf("%s\n", args[i]);
+    }
+}
+
+void reset_args(char** args)
+{
+    int i = 0;
+    while(args[i] != NULL)
+    {
+        free(args[i]);
+        i++;
+    }
+}
+
+
+
+
+int main(int argc, char** argv)
+{
+
+    char *buffer;
+    const char *delimitador = " \n";
+    char caracter;
+    char *token;
+    char *args[MAX_ARGS];
+    int contador_args = 0;
+    int anterior;
+    
+    pid_t pid;
+    const char *file_path;
+    int sigue = 1;
+    int estado_hijo = 0;
+    int indice_buffer = 0;
+    
+
+    if(argc > 1){
+        fprintf(stderr, "USO: shell [sin argumentos]\n"); 
+        return -1;
+    }
+    do
+    {   
+        
+        buffer = crear_buffer(); // Crea un nuevo puntero buffer
+        vaciar_buffer(buffer);
+        if(write(1, PROMPT, strlen(PROMPT)) == -1)
+        {
+            fprintf(stderr,"Error en la escritura.");
+            exit(EXIT_ERR);
+        }
+        
+        do
+        {      
+            
+            if(read(0 , &caracter, 1) != -1 && sigue)
+            {
+                
+                if(caracter == '\n') 
+                {
+                    sigue = 0;
+                }
+                buffer[indice_buffer] = caracter;  
+            }
+            indice_buffer++;
+        }
+        while (sigue);
+        buffer[indice_buffer] = '\0';
+        
+        
+        
+        
+            
+        // En este punto ya tenemos en el buffer la cadena obtenida por el usuario.
+        //Ahora comprobaremos si corresponde a alguna cadena de comando
+        
+        if(buffer[0] != '\n')
+        {
+            token = strtok(buffer, delimitador); // Cojo el primer argumento
+            strcat(token, "\0");
+        }
+        else { token = NULL; }
+        while(token != NULL && contador_args < MAX_ARGS)
+        {    
+            args[contador_args] = (char*)malloc((strlen(token)+1) * sizeof(char));
+            strcpy(args[contador_args], token);
+            contador_args++;
+            token = strtok(NULL, delimitador);
+            strcat(token, "\0");
+            
+            
+        }
+        //printf("%s\n", buffer);
+        print_args(args, anterior);
+        if(args[0] != NULL)
+        {
+            
+            if(!strcmp(args[0], "copia"))
+            {
+                pid = fork();
+                if(pid < 0)
+                {
+                    fprintf(stderr, "No se ha podido crear el hijo.");
+                    return -1;
+                }
+                else if(pid == 0)
+                {
+                    file_path = "./copia";
+                    estado_hijo = execvp(file_path, args);
+                    if(estado_hijo)
+                    {
+                        fprintf(stderr, "Error en la ejecucion del proceso hijo.\n");
+                        return -1;
+                    }
+                }
+                else
+                {   
+                    wait(NULL);
+                }
+            } 
+            else if (!strcmp(args[0], "muestra"))
+            {
+                pid = fork();
+                if(pid < 0)
+                {
+                    fprintf(stderr, "Error al crear el proceso hijo.\n");
+                    return -1;
+                }
+                else if(pid == 0)
+                {
+                    file_path = "/bin/cat";
+                    strcpy(args[0], file_path);
+                    //print_args(args, contador_args);
+                    estado_hijo = execvp(file_path, args);
+                    if(estado_hijo)
+                    {
+                        fprintf(stderr, "Error en la ejecucion del proceso hijo.\n");
+                    }
+                }
+                else
+                {
+                    wait(NULL);
+                }
+            }
+            else if(!strcmp(args[0], "lista"))
+            {
+                pid = fork();
+                if(pid < 0)
+                {
+                    fprintf(stderr, "Error al crear el proceso hijo.\n");
+                    return -1;
+                }
+                else if(pid == 0)
+                {
+                    file_path = "/bin/ls";
+                    strcpy(args[0], file_path);
+                    estado_hijo = execvp(file_path, args);
+                    if(estado_hijo)
+                    {
+                        fprintf(stderr, "Error en la ejecucion del proceso hijo.\n");
+                    }
+                }
+                else
+                {
+                    wait(NULL);
+                }
+            }
+            else if(!strcmp(args[0], "salir"))
+            {
+                reset_args(args);
+                free(buffer);
+                return 0; // Termina el programa
+            }
+            else
+            {
+                printf("Comando desconocido\n");
+            }
+        }
+        
+        eliminar_buffer(buffer);
+        reset_args(args);
+        sigue = 1;
+        anterior = contador_args;
+        indice_buffer = 0;
+        contador_args = 0;
+        
+    
+    }while(1);
+
+}
