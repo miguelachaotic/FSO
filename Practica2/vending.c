a37ab9f128eda2e157c3d819207f3dd7ca129750
Miguel Ángel <119065677+miguelachaotic@users.noreply.github.com>
2023-12-06 19:53:40 +0100

    Update vending.c VARIOS PRODUCTORES UN CONSUMIDOR FUNCIONAL

--- Practica2/vending.c
+++ Practica2/vending.c
@@ -1,197 +1,443 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include <string.h>
 #include <ctype.h>
-#include <semaphore.h>
 
-#define SIZE_BUFFER 100
+#define MAX_PRODUCTORES 7
+#define MAX_CONSUMIDORES 1000
+#define MAX_BUFFER 5000
 
-typedef struct dato
+
+typedef struct dato_buffer
 {
-    char caracter;
+    char c;
     int id_hilo;
-
 } DATO_BUFFER;
 
-int es_num(char* string);
+typedef struct dato_productor
+{
+    int id_hilo; // identificación del hilo que produce datos
+    char *path;
+    FILE* fichero_salida; // Puntero al fichero donde escribe datos
+
+
+} DATO_PRODUCTOR;
+
+typedef struct dato_lista_enlazada
+{
+    int total;
+    int tipos[10]; // Los tipos de datos que se han consumido
+    int id_proveedor[7]; // Solo puede haber como mucho 7 productores
+
+} DATO_LISTA_ENLAZADA;
+
+
+
+int esnum(char* cadena);
+
 void* productor(void* args);
+
 void* consumidor(void* args);
-DATO_BUFFER* crear_dato(FILE *fp, int id);
 
-/* Variables globales */
+void* facturador(void* args);
 
-sem_t hay_espacio, hay_dato;
-int validos, no_validos;
-DATO_BUFFER* buffer;
+DATO_BUFFER crear_dato_buffer(FILE* fp, int id_hilo);
+
+int caracter_valido(char c);
+
+
+void escribe_lista_enlazada(DATO_LISTA_ENLAZADA nuevo_dato);
+
+DATO_BUFFER* buffer_compartido;
+
+sem_t hay_dato;
+
+sem_t hay_espacio;
+
+sem_t mutex_indice_productores;
+
+sem_t mutex_indice_consumidores;
+
+sem_t mutex_contador_productores;
+
+sem_t mutex_fichero_salida;
+
+sem_t mutex_indice_lista_enlazada;
+
+int indice_productores = 0;
+
+int indice_consumidores = 0;
+
+int indice_lista_enlazada;
+
+int contador_productores;
+
+
+int tam_buffer;
+
+
+DATO_LISTA_ENLAZADA lista_enlazada[1000]; //Para hacer pruebas usamos un vector primero
 
 int main(int argc, char** argv)
 {
-    // uso: ./program files output Tam Prov Cons
-    int tamaño_buffer, num_proveed, num_consum;
-    char* path;
-    FILE* archivo_salida;
-    FILE** archivos_entrada;
-
-    // Comprobamos cantidad de argumentos
-    if (argc != 6) {
-        fprintf(stderr, "ERROR: nº de argumentos incorrecto\nUso: ./program input output Tam Prov Cons\n");
-        exit (1);
-    }
-    // Comprobamos que los argumentos sean correctos
-    if (!es_num(argv[3])) {
-        fprintf(stderr, "ERROR: argumento <tamaño_buffer> no es un número\n");
-        exit (1);
-    }
-    if (!es_num(argv[4])) {
-        fprintf(stderr, "ERROR: argumento <num_proveedores> no es un número\n");
-        exit (1);
-    }
-    if (!es_num(argv[5])) {
-        fprintf(stderr, "ERROR: argumento <num_consumidores> no es un número\n");
-        exit (1);
-    }
-
-    tamaño_buffer = atoi(argv[3]);
-    num_proveed = atoi(argv[4]);
-    num_consum = atoi(argv[5]);
-    if (tamaño_buffer < 1 || tamaño_buffer > 5000) {
-        fprintf(stderr, "ERROR: argumento <tamaño_buffer> fuera de limites [1,5000]\n");
-        exit (1);
-    }
-    if (num_proveed < 1 || num_proveed > 7) {
-        fprintf(stderr, "ERROR: argumento <num_proveedores> fuera de limites [1,7]\n");
-        exit (1);
-    }
-    if (num_consum < 1 || num_consum > 1000) {
-        fprintf(stderr, "ERROR: argumento <num_consum> fuera de limites [1,1000]\n");
-        exit (1);
-    }
-
-    // Comprobamos archivos (salida y entrada)
-    if ((archivo_salida = fopen(argv[2], "w")) != NULL) {
-        fclose(archivo_salida);
-        printf("WARNING: El archivo de salida se sobreescribirá\n");
-    }
-    if ((archivo_salida = fopen(argv[2], "a")) == NULL) {
-        fprintf(stderr, "Error en apertura del archivo de salida\n");
-        exit (1);
-    }
-
-    path = (char *)malloc(50);
-    archivos_entrada = (FILE **)malloc(sizeof(FILE *));
-    for (int i = 0; i < num_proveed; i++) {
-        sprintf(path, "%s/proveedor%d.dat%c", argv[1], i, '\0');
-        if ((archivos_entrada[i] = fopen(path, "r")) == NULL) {
-            fprintf(stderr, "Error abriendo los archivos de entrada (%s)\n", path);
-            exit (1);
-        }
-    }
-    return 0;
-}
+    int num_consumidores, num_productores, ids_consumidores[1000];
+    FILE* fichero_salida;
+    FILE ** ficheros_entrada;
+    char ruta_fichero[256]; // Almacena la ruta (relativa o absoluta) a un fichero.
+    pthread_t *hilos_productores, *hilos_consumidores, hilo_facturador;
+    DATO_PRODUCTOR *datos_productores;
 
 
-void* productor(void* args) 
-{
-    int n;
-    DATO_BUFFER* dato;
-    FILE *fp;
-    int i = 0;
-    n = *((int*)args);
-    if(args == NULL)
+    // Es un dato temporal, se reutiliza con cada proveedor distinto
+
+    if(argc != 6)
     {
-        fprintf(stderr, "Error en los argumentos\n");
-        pthread_exit(NULL);
+        fprintf(stderr, "Uso incorrecto de la vending.\nUso correcto:\n ./vending <path> <outputFile> <T> <P> <C>\n");
+        exit(1);
     }
-    if((fp = fopen("../datos_fso/proveedor9.dat", "r")) == NULL)
+    if(!esnum(argv[3]))
     {
-        fprintf(stderr, "No se pudo abrir el archivo\n");
-        pthread_exit(NULL);
+        fprintf(stderr, "El tamaño del buffer no es una cadena válida: %s\n", argv[3]);
+        exit(1);
     }
-    while(1){
-        dato = crear_dato(fp, n);
-        
-        sem_wait(&hay_espacio); //Espera a que haya un espacio
-        
-        buffer[i] = *dato;
-
-        sem_post(&hay_dato); //Señala que hay dato
-        if(dato->caracter == EOF)
+    tam_buffer = atoi(argv[3]);
+    if(tam_buffer < 1 || tam_buffer > MAX_BUFFER)
+    {
+        fprintf(stderr, "Tamaño inválido para el buffer circular.\n");
+        exit(1);
+    }
+    if(!esnum(argv[4]))
+    {
+        fprintf(stderr, "Cadena es alfanúmerica, no válida. %s\n", argv[4]);
+        exit(1);
+    }
+    num_productores = atoi(argv[4]);
+    if(num_productores < 1 || num_productores > MAX_PRODUCTORES)
+    {
+        fprintf(stderr, "Número inválido de proveedores. Has introducido %d\n", num_productores);
+        exit(1);
+    }
+    if(!esnum(argv[5]))
+    {
+        fprintf(stderr, "La cadena introducida es alfanúmerica, no válida. %s\n", argv[5]);
+        exit(1);
+    }
+    num_consumidores = atoi(argv[5]);
+    if(num_consumidores < 1 || num_consumidores > MAX_CONSUMIDORES)
+    {
+        fprintf(stderr, "Número inválido de proveedores. Has introducido %d\n", num_consumidores);
+        exit(1);
+    }
+    // Abrimos el archivo en lectura para comprobar que existe
+    if((fichero_salida = fopen(argv[2], "w+")) != NULL)
+    {
+        if(fclose(fichero_salida))
         {
-            printf("Termina productor\n");
-            pthread_exit(NULL);
+            fprintf(stderr, "Error al cerrar el fichero de salida.\n");
+            exit(1);
         }
-        free(dato);
-        i = (i+1)%SIZE_BUFFER;
+        printf("WARNING: EL ARCHIVO SELECCIONADO SE SOBREESCRIBIRÁ.\n");
     }
-}
 
-
-void* consumidor(void* args) {
-    DATO_BUFFER dato;
-    int contador[10];
-    int j = 0;
-    int sigue = 1;
-    if(args != NULL)
+    if((fichero_salida = fopen(argv[2], "a")) == NULL)
     {
-        fprintf(stderr, "Error en los argumentos\n");
-        pthread_exit(NULL);
+        fprintf(stderr, "Error en la apertura del archivo de salida.\n");
+        exit(1);
     }
-    while(sigue)
+
+    if((ficheros_entrada = (FILE**) malloc(num_productores*sizeof(FILE*))) == NULL)
     {
-        sem_wait(&hay_dato); //Espera a que haya un dato
-
-        dato = buffer[j];
-
-        sem_post(&hay_espacio); // Señala que hay espacio
-        if(dato.caracter == EOF)
+        fprintf(stderr, "Error al crear el vector de punteros de FILE.\n");
+        exit(1);
+    }
+    for(int i = 0; i < num_productores; i++)
+    {
+        if(sprintf(ruta_fichero, "%s/proveedor%d.dat",argv[1], i) < 0)
         {
-            sigue = 0;
+            fprintf(stderr, "Error al crear el string de la ruta al archivo número %d.\n", i);
+            exit(1);
         }
-        
-
-        contador[dato.caracter-'a']++;
-
-        j = (j+1)%SIZE_BUFFER;
-
-    }
-    printf("Objetos válidos: %d, objetos no válidos: %d, total: %d\n", validos, no_validos, validos+no_validos);
-    for(int i = 0; i < 10; i++){
-        printf("Objeto tipo \"%c\":, cantidad %d\n", (i + 'a'), contador[i]);
-    }
-
-    printf("Termina consumidor\n");
-    pthread_exit(NULL);
-    
-}
-
-DATO_BUFFER* crear_dato(FILE *fp, int id)
-{
-    DATO_BUFFER *nuevo_dato;
-    char chr = 0;
-    nuevo_dato = (DATO_BUFFER*)malloc(sizeof(DATO_BUFFER));
-    while(chr != EOF)
-    {
-        chr = fgetc(fp);
-        if((chr >= 'a' && chr <= 'j') || chr == EOF){
-            nuevo_dato->caracter = chr;
-            nuevo_dato->id_hilo = id;
-            validos++;
-            return nuevo_dato;
-        } 
-        else 
+        if((ficheros_entrada[i] = fopen(ruta_fichero, "r")) == NULL)
         {
-            no_validos++;
+            fprintf(stderr, "Error al abrir el fichero %d\n", i);
+            exit(1);
+        }
+        if(fclose(ficheros_entrada[i]))
+        {
+            fprintf(stderr, "Error al cerrar fichero de entrada número %d.\n", i);
+            exit(1);
         }
     }
-    return NULL;
+    free(ficheros_entrada);
+    //En ficheros_entrada están todos los punteros a FILE (FILE*) que ha pedido el usuario
+
+    // Buffer de tamaño tam_buffer de elementos tipo DATO
+    if((buffer_compartido = (DATO_BUFFER*)malloc(tam_buffer*sizeof(DATO_BUFFER))) == NULL)
+    {
+        fprintf(stderr, "Error en la creación del buffer compartido.\n");
+        exit(1);
+    }
+
+    if((hilos_productores = (pthread_t*)malloc(num_productores*sizeof(pthread_t))) == NULL)
+    {
+        fprintf(stderr, "Error en la creación del vector de hilos productores.\n");
+        exit(1);
+    }
+
+    if((hilos_consumidores = (pthread_t*)malloc(num_consumidores*sizeof(pthread_t))) == NULL)
+    {
+        fprintf(stderr, "Error en la creación del vector de hilos consumidores.\n");
+        exit(1);
+    }
+
+    if((datos_productores = (DATO_PRODUCTOR*)malloc(num_productores*sizeof(DATO_PRODUCTOR))) == NULL)
+    {
+        fprintf(stderr, "Error en la creación del vector de datos de los productores.\n");
+        exit(1);
+    }
+
+    contador_productores = num_productores;
+    sem_init(&hay_espacio, 0, tam_buffer);
+    sem_init(&hay_dato, 0, 0);
+    sem_init(&mutex_contador_productores, 0, num_productores);
+    sem_init(&mutex_indice_productores, 0, 1);
+    sem_init(&mutex_indice_consumidores, 0, 1);
+
+    // Lanzamos los hilos productores
+    for(int i = 0; i < num_productores; i++)
+    {
+        datos_productores[i].fichero_salida = fichero_salida;
+        datos_productores[i].id_hilo = i + 1;
+        datos_productores[i].path = argv[1]; //Copiamos el puntero a argv[1]
+        printf("Id del hilo: %d\n", datos_productores[i].id_hilo);
+        if(pthread_create(&hilos_productores[i], NULL, productor, (void*) &datos_productores[i]))
+        {
+            fprintf(stderr, "No se ha podido crear el hilo productor número %d.\n", i);
+            exit(1);
+        }
+    }
+
+    // Lanzamos los hilos consumidores
+
+    for(int i = 0; i < num_consumidores; i++)
+    {
+        ids_consumidores[i] = i + 1;
+        if(pthread_create(&hilos_consumidores[i], NULL, consumidor, (void*) &ids_consumidores[i]))
+        {
+            fprintf(stderr, "Error en la creación del hilo consumidor número %d.\n", i);
+            exit(1);
+        }
+    }
+
+
+    // Lanzamos hilo facturador
+    if(pthread_create(&hilo_facturador, NULL, facturador, (void*)fichero_salida))
+    {
+        fprintf(stderr, "Error al crear el hilo facturador.\n");
+        exit(1);
+    }
+
+    // Esperamos a los productores
+    for(int i = 0; i < num_productores; i++)
+    {
+        if(pthread_join(hilos_productores[i], NULL))
+        {
+            fprintf(stderr, "Error al esperar al hilo productor número %d\n", i+1);
+            exit(1);
+        }
+    }
+    free(hilos_productores);
+    free(datos_productores);
+
+    // Esperamos a los consumidores
+    for(int i = 0; i < num_consumidores; i++)
+    {
+        if(pthread_join(hilos_consumidores[i], NULL))
+        {
+            fprintf(stderr, "Error al esperar al hilo consumidor número %d.\n", i+1);
+            exit(1);
+        }
+    }
+
+    free(hilos_consumidores);
+    free(buffer_compartido);
+
+
+    // Esperamos al facturador
+    if(pthread_join(hilo_facturador, NULL))
+    {
+        fprintf(stderr, "Error al esperar al hilo facturador.\n");
+        exit(1);
+    }
+
+    // Cerramos el fichero de salida
+    if(fclose(fichero_salida))
+    {
+        fprintf(stderr, "Error al cerrar el fichero de salida.\n");
+        exit(1);
+    }
+
+
+    exit(0);
+
 }
 
-int es_num(char* string)
+
+int esnum(char* cadena)
 {
-    for (int i = 0; i <= strlen(string); i++) {
-        if (!isdigit(string[i]) && string[i] != '\0')
-            return (0);
+    for(int i = 0; i < strlen(cadena); i++)
+    {
+        if(!isdigit(cadena[i]) && cadena[i] != '\n')
+        {
+            return 0;
+        }
     }
     return 1;
 }
+
+void* productor(void* args)
+{
+    DATO_PRODUCTOR dato_args;
+    DATO_BUFFER nuevo_dato;
+    FILE* fp;
+    char ruta_fichero[256];
+    int sigue = 1;
+
+    if(args == NULL)
+    {
+        fprintf(stderr, "Error en los argumentos de la función.\n");
+        exit(1);
+    }
+    dato_args = *((DATO_PRODUCTOR*) args);
+    if(sprintf(ruta_fichero, "%s/proveedor%d.dat", dato_args.path, dato_args.id_hilo-1) < 0)
+    {
+        fprintf(stderr, "Error al crear el path en el hilo %d.\n", dato_args.id_hilo);
+        exit(1);
+    }
+    if((fp = fopen(ruta_fichero, "r"))== NULL)
+    {
+        fprintf(stderr, "Error al abrir el fichero proveedor%d.dat.\n", dato_args.id_hilo-1);
+        exit(1);
+    }
+    while(sigue)
+    {
+        nuevo_dato = crear_dato_buffer(fp, dato_args.id_hilo);
+        sem_wait(&hay_espacio);
+        sem_wait(&mutex_indice_productores);
+        buffer_compartido[indice_productores] = nuevo_dato;
+        sem_post(&mutex_indice_productores);
+        sem_post(&hay_dato);
+        printf("He escrito el caracter %c en el buffer con id %d.\n", nuevo_dato.c, nuevo_dato.id_hilo);
+        if(nuevo_dato.id_hilo != -1)
+        {
+            indice_productores = (indice_productores + 1)%tam_buffer;
+        }
+        else
+        {
+            sigue = 0;
+            printf("Productor termina.\n");
+        }
+    }
+
+    if(fclose(fp))
+    {
+        fprintf(stderr, "Error al cerrar el fichero número %d.\n", dato_args.id_hilo-1);
+        exit(1);
+    }
+    pthread_exit(NULL);
+}
+
+void* consumidor(void* args)
+{
+    DATO_LISTA_ENLAZADA nuevo_dato;
+    DATO_BUFFER dato_buffer;
+    int id_hilo = *(int*) args;
+    int sigue = 1;
+    int contador[10];
+    while(sigue)
+    {
+        sem_wait(&mutex_contador_productores);
+        if(contador_productores == 0)
+        {
+            sem_post(&mutex_contador_productores);
+            sigue = 0;
+            // Escribir aquí estadísticas en la lista enlazada
+            // y eso
+            printf("Consumidor termina.\n");
+        }
+        else
+        {
+            sem_post(&mutex_contador_productores);
+            sem_wait(&hay_dato);
+            sem_wait(&mutex_indice_consumidores);
+            dato_buffer = buffer_compartido[indice_consumidores];
+            sem_post(&mutex_indice_consumidores);
+            sem_post(&hay_espacio);
+            printf("Consumidor %d lee %c con id: %d\n",id_hilo, dato_buffer.c, dato_buffer.id_hilo);
+            if(dato_buffer.c == EOF)
+            {
+                sem_wait(&mutex_contador_productores);
+                contador_productores--;
+                sem_post(&mutex_contador_productores);
+            }
+            sem_wait(&mutex_indice_consumidores);
+            indice_consumidores = (indice_consumidores + 1)%tam_buffer;
+            sem_post(&mutex_indice_consumidores);
+            contador[dato_buffer.c - 'a']++;
+        }
+    }
+    pthread_exit(NULL);
+}
+
+void* facturador(void* args)
+{
+    printf("Facturador escribe en fichero de salida %ld\n", *(long*) args);
+    printf("Facturador terminado.\n");
+    pthread_exit(NULL);
+}
+
+DATO_BUFFER crear_dato_buffer(FILE* fp, int id_hilo)
+{
+    DATO_BUFFER nuevo_dato;
+    char c = 0;
+    if (fp == NULL)
+    {
+        fprintf(stderr, "El puntero a fichero no puede ser NULL.\n");
+        exit(1);
+    }
+
+    while(1)
+    {
+        nuevo_dato.id_hilo = -1;
+        c = (char)fgetc(fp);
+        if(caracter_valido(c)) {
+            nuevo_dato.id_hilo = id_hilo;
+            nuevo_dato.c = c;
+            return nuevo_dato;
+        }
+        else if(c == EOF)
+        {
+            nuevo_dato.id_hilo = -1;
+            nuevo_dato.c = c;
+            return nuevo_dato;
+        }
+
+
+    }
+    return nuevo_dato;
+
+
+}
+
+int caracter_valido(char c)
+{
+    return (c >= 'a' && c <= 'j') ? 1 : 0;
+}
+
+void escribe_lista_enlazada(DATO_LISTA_ENLAZADA nuevo_dato)
+{
+
+}
+
